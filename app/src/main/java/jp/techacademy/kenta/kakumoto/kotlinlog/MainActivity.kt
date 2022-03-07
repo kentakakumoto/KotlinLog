b47323336e8fae1cf4716676fc52f30e9ab05e97
kentakakumoto <gotta3ken@gmail.com>
2022-03-07 21:55:32 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/kenta/kakumoto/kotlinlog/MainActivity.kt
@@ -0,0 +1,34 @@
+package jp.techacademy.kenta.kakumoto.kotlinlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("kotlintest","ログへの出力テスト")
+
+        val str1 = "Hello"
+        val str2 = "World"
+        val str3 = "Hello"
+
+        if(str1.equals(str2)){
+            Log.d("kotlintest","str1とstr2は一緒です。")
+        }else{
+            Log.d("kotlintest", "str1とstr2は異なります。")
+        }
+
+        if(str1.equals(str3)){
+            Log.d("kotlintest","str1とstr3は一緒です。")
+        }else{
+            Log.d("kotlintest", "str1とstr3は異なります。")
+        }
+
+
+
+       }
+
+}
---
+++ app/src/main/java/jp/techacademy/kenta/kakumoto/kotlinlog/Movable.kt
@@ -0,0 +1,5 @@
+package jp.techacademy.kenta.kakumoto.kotlinlog
+
+interface Movable {
+    fun move()
+}
